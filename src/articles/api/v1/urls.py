6000852fe5f487f3d31ad91d5f038f01bdda9da4
Andre Pereira de Souza Duarte <andreduarte2010@gmail.com>
2018-10-05 23:41:52 -0300

    Feat: added likes counter to article serializer and added likes backend

--- src/articles/api/v1/urls.py
+++ src/articles/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import ArticleViewSet
+from .views import ArticleViewSet,ArticleLikesDetailsViewSet, ArticleLikesListViewSet
 
 
 article_router = routers.SimpleRouter()
@@ -9,5 +9,20 @@ article_router.register('articles', ArticleViewSet, base_name='articles')
 
 
 urlpatterns = [
-    path('', include(article_router.urls))
+    path('', include(article_router.urls)),
+    path(
+        r'article/likes/<pk>/',
+        ArticleLikesDetailsViewSet.as_view({
+            'get': 'retrieve',
+            'delete': 'destroy'
+            }),
+        name='article-detail'
+    ),
+    path(
+        r'article/likes/', ArticleLikesListViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+        }),
+        name='article-list'
+    ),
 ]
--- src/articles/api/v1/views.py
+++ src/articles/api/v1/views.py
@@ -1,8 +1,10 @@
 from django.utils import timezone
 from rest_framework import viewsets, mixins
 
-from articles.models import Article
-from .serializers import ArticleSerializer
+from articles.models import Article, ArticleLikes
+from rest_framework.pagination import LimitOffsetPagination
+
+from .serializers import ArticleSerializer, ArticleLikesSerializer
 
 
 class ArticleViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -14,3 +16,38 @@ class ArticleViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 
     def get_queryset(self):
         return Article.objects.filter(created__lt=timezone.now()).order_by('-publish_date')
+
+
+class ArticleLikesDetailsViewSet(viewsets.ModelViewSet):
+    """
+    hero:
+
+    """
+    serializer_class = ArticleLikesSerializer
+
+    def get_queryset(self):
+        return ArticleLikes.objects.all()
+
+class ArticleLikesListViewSet(viewsets.ModelViewSet):
+    """
+    hero:
+
+    """
+    serializer_class = ArticleLikesSerializer
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        query_params = self.request.query_params
+        user= 'user' in  query_params and query_params['user']
+        article = 'article' in query_params and query_params['article']
+        if user and article :
+            return ArticleLikes.objects.filter(user__pk=user).filter(article__pk=article)
+        elif user :
+            return ArticleLikes.objects.filter(article__pk=article)
+        elif article:
+            return ArticleLikes.objects.filter(user__pk=user)
+        else:
+            return ArticleLikes.objects.all()
+
+
+
